5514a6ac1b132ad51ec1cc442e695591cb55461d
AleksaPapovic <papovic.ra167.2018@uns.ac.rs>
2024-02-08 20:49:14 +0100

    feat: cards node

--- node-api/routes/card.js
+++ node-api/routes/card.js
@@ -7,10 +7,9 @@ const router = Router();
 
 router.post("/create-card", async (req, res) => {
     try {
-      let id;
-        let personId;
-        let balance;
-        let currency;
+        let cardNumber;
+        let id;
+        let bankAccountId;
   
       try {
         id = req.body["id"];
@@ -20,33 +19,25 @@ router.post("/create-card", async (req, res) => {
       }
   
       try {
-        personId = req.body["personId"];
-        if (personId.length < 1) throw "";
+        cardNumber = req.body["cardNumber"];
+        if (cardNumber.length < 1) throw "";
       } catch (_) {
-        return res.status(400).send({ message: "PersonId is a mandatory field!" });
+        return res.status(400).send({ message: "cardNumber is a mandatory field!" });
       }
   
       try {
-        balance = req.body["balance"];
-        if (balance.length < 1) throw "";
+        bankAccountId = req.body["bankAccountId"];
+        if (bankAccountId.length < 1) throw "";
       } catch (_) {
-        return res.status(400).send({ message: "Balance is a mandatory field!" });
-      }
-  
-      try {
-        currency = req.body["currency"];
-        if (currency.length < 1) throw "";
-      } catch (_) {
-        return res.status(400).send({ message: "Currency is a mandatory field!" });
+        return res.status(400).send({ message: "bankAccountId is a mandatory field!" });
       }
   
       const contract = await getContract(req.org, req.channel);
       const result = await contract.submitTransaction(
-        "CreateBankAccount",
+        "CreateCard",
+        cardNumber,
         id,
-        personId,
-        currency,
-        balance
+        bankAccountId
       );
       try {
         return res.send(prettyJSONString(result));
@@ -62,46 +53,28 @@ router.post("/create-card", async (req, res) => {
 
   router.post("/remove-card", async (req, res) => {
     try {
-      let id;
-        let personId;
-        let balance;
-        let currency;
+        let id;
+        let bankAccountId;
   
       try {
         id = req.body["id"];
         if (id.length < 1) throw "";
       } catch (_) {
-        return res.status(400).send({ message: "Id is a mandatory field!" });
+        return res.status(400).send({ message: "id is a mandatory field!" });
       }
   
       try {
-        personId = req.body["personId"];
-        if (personId.length < 1) throw "";
+        bankAccountId = req.body["bankAccountId"];
+        if (bankAccountId.length < 1) throw "";
       } catch (_) {
-        return res.status(400).send({ message: "PersonId is a mandatory field!" });
-      }
-  
-      try {
-        balance = req.body["balance"];
-        if (balance.length < 1) throw "";
-      } catch (_) {
-        return res.status(400).send({ message: "Balance is a mandatory field!" });
-      }
-  
-      try {
-        currency = req.body["currency"];
-        if (currency.length < 1) throw "";
-      } catch (_) {
-        return res.status(400).send({ message: "Currency is a mandatory field!" });
+        return res.status(400).send({ message: "bankAccountId is a mandatory field!" });
       }
   
       const contract = await getContract(req.org, req.channel);
       const result = await contract.submitTransaction(
-        "CreateBankAccount",
+        "RemoveCard",
         id,
-        personId,
-        currency,
-        balance
+        bankAccountId
       );
       try {
         return res.send(prettyJSONString(result));
